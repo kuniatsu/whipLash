981e71194b9fbf28ce2365f9a91023d0b5141a7b
KUNISHIMA ATSUSHI <kuniatsu@mbp.local>
2020-01-11 01:17:29 +0900

    #31 iOS&Android改行じゃなくてもタスク追加

--- public/index.js
+++ public/index.js
@@ -17,6 +17,18 @@ let config;
         handle:".listIcon",
         animation: 100
     });
+
+    //デバイス確認
+    var getDevice = (()=>{
+        var ua = navigator.userAgent;
+        if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0){
+            return 'sp';
+        }else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0){
+            return 'tab';
+        }else{
+            return 'other';
+        }
+    })();
 })();
 
 function arrayShuffle(arg){
@@ -99,7 +111,15 @@ function setEvent(){
     inputTask.addEventListener('keypress', (e)=> {
         var ele = checkKeyPress(e,13,makeTask);
     });
+    //スマホだった場合フォーカスが外れるだけで入力したい
+    inputTask.addEventListener('blur', (e)=> {
+        if(getDevice=="sp"||getDevice=="tab"){
+            console.log("spかつフォーカス外れた!");
+            makeTask();
+        }
+    });   
 }
+
 
 
 /**
